b0292e61622d2515b4f1c04a0826f50939bfb425
yayoc <n.yayoshi+github@gmail.com>
2018-06-24 22:21:22 +0900

    Split files

---
+++ src/compose.ts
@@ -0,0 +1,42 @@
+import { ResponsesType, APPLICATION_JSON } from "./response";
+import { Schemas } from "./schema";
+import { normalizePath, getSchemaName } from "./util";
+import { REF, parseObject, parseArray } from "./parse";
+import { isObject, isArray } from "./dataType";
+
+type MockData = {
+  [path: string]: any;
+};
+
+// Compose mock data
+export const composeMockData = (
+  responses: ResponsesType,
+  schemas: Schemas
+): MockData => {
+  let ret: any = {};
+  Object.keys(responses).forEach(path => {
+    const res: any = responses[path];
+    const pathKey = normalizePath(path);
+    if (res) {
+      const val = res[APPLICATION_JSON];
+      const { schema } = val;
+      const ref = schema[REF];
+      if (ref) {
+        const schemaName = getSchemaName(ref);
+        if (schemaName) {
+          const values = schemas[schemaName];
+          ret[pathKey] = values;
+        }
+      } else {
+        if (isObject(schema)) {
+          ret[pathKey] = parseObject(schema, schemas);
+        } else if (isArray(schema)) {
+          ret[pathKey] = parseArray(schema, schemas);
+        } else {
+          ret[pathKey] = val.schema.properties;
+        }
+      }
+    }
+  });
+  return ret;
+};
